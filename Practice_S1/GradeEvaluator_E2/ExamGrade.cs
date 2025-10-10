61117ee2d9f2d65e8354b15e93286c1e1b484f90
ebursuc02 <eduarda.bursuc@endava.com>
2025-10-10 16:52:08 +0300

    Delete Practice_S1 directory

--- Practice_S1/GradeEvaluator_E2/ExamGrade.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GradeEvaluator_E2;
-
-public class ExamGrade : Grade
-{
-    public string ExamType { get; init; } = string.Empty;
-    public bool Passed { get => Score >= 5; }
-}
--- Practice_S1/GradeEvaluator_E2/Grade.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GradeEvaluator_E2;
-
-public class Grade
-{
-    public required int Score { get; init; }
-    public DateTime Date { get; } = DateTime.UtcNow;
-    public required string Subject { get; init; } = string.Empty;
-}

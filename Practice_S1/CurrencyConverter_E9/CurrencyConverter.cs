cee0211990c94a59eebb7dc7fb37375982034411
ebursuc02 <eduarda.bursuc@endava.com>
2025-10-14 13:53:47 +0300

    U: last exercises added

---
+++ Practice_S1/CurrencyConverter_E9/CurrencyConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace CurrencyConverter_E9;
+
+public class CurrencyConverter
+{
+    public List<CurrencyRate> currencyRates { get; } = [];
+
+    public void AddCurrencyRate(CurrencyRate currencyRate) => currencyRates.Add(currencyRate);
+
+    public void Get30DaysReport()
+    {
+        List<DateTime> days = Enumerable
+            .Range(0, 30)
+            .Select(i => DateTime.UtcNow.AddDays(-i))
+            .ToList();
+
+        Console.WriteLine($"| Date  |  USD  |  EUR  |  GBP  |");
+
+        foreach (var day in days)
+        {
+            var usdRate = currencyRates.Where(r => r.Date.Date == day.Date && r.Currency == "USD").FirstOrDefault();
+            var eurRate = currencyRates.Where(r => r.Date.Date == day.Date && r.Currency == "EUR").FirstOrDefault();
+            var gbpRate = currencyRates.Where(r => r.Date.Date == day.Date && r.Currency == "GBP").FirstOrDefault();
+            Console.WriteLine($"| {day:dd.MM} | {usdRate?.Rate.ToString("f2") ?? "    "}  | {eurRate?.Rate.ToString("f2") ?? "    "}  | {gbpRate?.Rate.ToString("f2") ?? "    "}  |");
+        }
+            
+    }
+
+}
---
+++ Practice_S1/SecureDivision_E12/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    Console.Write("Enter the first number: ");
+    decimal a = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Enter the second number: ");
+    decimal b = Convert.ToDecimal(Console.ReadLine());
+
+    if (b == 0)
+    {
+        Console.WriteLine("Error: division by 0 not allowed.");
+        return;
+    }
+
+    decimal result = a / b;
+
+    result = Math.Round(result, 2, MidpointRounding.ToEven);
+
+    Console.WriteLine($"Result a/b: {result}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: number formats are incorrect.");
+}
